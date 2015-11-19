5e09c2fdbbd692fb68da002ebfc742e51adecdfe
rahul samaddar <rahul.samaddar@jktech.com>
2015-11-19 20:16:38 +0530

    library classes for xls exporter

---
+++ routes/xlsExporter.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+var request = require('request');
+
+// Models
+var models = require('../models/exporter');
+
+// Export xls
+router.route('/bibles/:bible_id/books/:book_id/chapters/:chapter_id/xlsx').post(function(req, res){
+
+      models.Exporter.execute(req.body, function(executeErr, exportData) {
+        if (executeErr && executeErr.name == 'ValidationError') { return res.status(400).json(executeErr); }
+        else if (executeErr) { return res.status(500).json(executeErr); }
+        return res.status(200).json({ url: exportData.url });
+      });
+
+
+});
+
+// Return router
+module.exports = router;
