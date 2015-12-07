43e136b4fba0bb3f0f46f0aa6b38ea31fef7a985
Joel Mathew <joelmathew.mec@gmail.com>
2015-12-07 00:51:18 -0800

    Adding more into test.

--- test/routes/export.js
+++ test/routes/export.js
@@ -5,7 +5,8 @@ Chapter = require('../../models/chapter'),
 Bible = require('../../models/bible'),
 Verse = require('../../models/verse'),
 setup = require('../setup'),
-app = require('../../server');
+app = require('../../server'),
+fs = require('fs');
 
 describe('export', function() {
 
@@ -41,16 +42,45 @@ describe('export', function() {
 	it('creates and downloads a chapter xlsx', function(done) {
 	    this.timeout(10000);
 	    request(app)
-		.post('/api/bibles/en-asv/books/Exodus/chapters/1/xlsx/hi')
+		.get('/api/bibles/en-asv/books/Exodus/chapters/1/xlsx/hi')
 		.set('Authorization', 'Token token=' + process.env.AUTH_TOKEN)
 		.accept('json')
 		.expect(200)
 		.end(function(err, res) {
 		    if (err) { return done(err);}
 		    expect(res.body).to.have.property('url');
+		    url = res.body.url
+		    console.log(url);
+		    download(url, '/tmp/temp.xlsx', function(err, file) {
+			if (err) done(err);
+			if (getFilesizeInBytes < 10) {
+			    fs.unlink('/tmp/temp.xlsx');
+			    done(err);
+			} else {
+			    fs.unlink('/tmp/temp.xlsx');
+			}
+		    });
 		    done();
 		});
 	});
+	var download = function(url, dest, cb) {
+	    var file = fs.createWriteStream(dest);
+	    var request = http.get(url, function(response) {
+		response.pipe(file);
+		file.on('finish', function() {
+		    file.close(cb(null, file));  // close() is async, call cb after close completes.
+		});
+	    }).on('error', function(err) { // Handle errors
+		fs.unlink(dest);
+		if (cb) cb(err.message);
+	    });
+	};
+
+	var getFilesizeInBytes = function (filename) {
+	    var stats = fs.statSync(filename)
+	    var fileSizeInBytes = stats["size"]
+	    return fileSizeInBytes
+	}
     });
 });
 
